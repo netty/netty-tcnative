netty/netty-tcnative

commit 2c17755b6330a6b6f2aa0dcb0ced5bca0ab476b3

Allow to correctly retrieve the number of handshakes for a ssl instance
Motivation:

In commit 1cd48327e711a230e325f74d3d09873d70043edd SSL.getHandshakeCount(...) was introduced. Unfortunally the implementation was not correct and so the returned number did not match the reality.

Modifications:

- Store the handshakeCount per SSL instance and so fix the implemented

Result:

SSL.getHandshakeCount(...) returns the correct number now.

src/main/c/ssl.c
@@ -719,8 +719,8 @@ TCN_IMPLEMENT_CALL(jint, SSL, initialize)(TCN_STDARGS, jstring engine)
      * low entropy seed.
      */
     SSL_rand_seed(NULL);
-    /* For SSL_get_app_data2() at request time */
-    SSL_init_app_data2_idx();
+    /* For SSL_get_app_data2() and SSL_get_app_data3() at request time */
+    SSL_init_app_data2_3_idx();
 
     SSL_TMP_KEYS_INIT(r);
     if (r) {
@@ -1178,11 +1178,11 @@ TCN_IMPLEMENT_CALL(jint, SSL, getLastErrorNumber)(TCN_STDARGS) {
 }
 
 static void ssl_info_callback(const SSL *ssl, int where, int ret) {
-    tcn_ssl_ctxt_t *c;
+    uint16_t *handshakeCount = NULL;
     if (0 != (where & SSL_CB_HANDSHAKE_START)) {
-        c = SSL_get_app_data2(ssl);
-        if (c != NULL) {
-            ++c->handshakeCount;
+        handshakeCount = (uint16_t*) SSL_get_app_data3(ssl);
+        if (handshakeCount != NULL) {
+            ++(*handshakeCount);
         }
     }
 }
@@ -1191,6 +1191,7 @@ TCN_IMPLEMENT_CALL(jlong /* SSL * */, SSL, newSSL)(TCN_STDARGS,
                                                    jlong ctx /* tcn_ssl_ctxt_t * */,
                                                    jboolean server) {
     tcn_ssl_ctxt_t *c = J2P(ctx, tcn_ssl_ctxt_t *);
+    uint16_t *handshakeCount = malloc(sizeof(uint16_t));
     SSL *ssl;
 
     UNREFERENCED_STDARGS;
@@ -1201,6 +1202,11 @@ TCN_IMPLEMENT_CALL(jlong /* SSL * */, SSL, newSSL)(TCN_STDARGS,
         tcn_ThrowException(e, "cannot create new ssl");
         return 0;
     }
+
+    // Store the handshakeCount in the SSL instance.
+    *handshakeCount = 0;
+    SSL_set_app_data3(ssl, handshakeCount);
+
     // Add callback to keep track of handshakes.
     SSL_CTX_set_info_callback(c->ctx, ssl_info_callback);
 
@@ -1313,7 +1319,14 @@ TCN_IMPLEMENT_CALL(void, SSL, freeSSL)(TCN_STDARGS,
                                        jlong ssl /* SSL * */) {
     UNREFERENCED_STDARGS;
 
-    SSL_free(J2P(ssl, SSL *));
+    uint16_t *handshakeCount = NULL;
+    SSL *ssl_ = J2P(ssl, SSL *);
+
+    handshakeCount = SSL_get_app_data3(ssl_);
+    if (handshakeCount != NULL) {
+        free(handshakeCount);
+    }
+    SSL_free(ssl);
 }
 
 // Make a BIO pair (network and internal) for the provided SSL * and return the network BIO
@@ -1751,7 +1764,7 @@ TCN_IMPLEMENT_CALL(jbyteArray, SSL, getSessionId)(TCN_STDARGS, jlong ssl)
 
 TCN_IMPLEMENT_CALL(jint, SSL, getHandshakeCount)(TCN_STDARGS, jlong ssl)
 {
-    tcn_ssl_ctxt_t *c;
+    uint16_t *handshakeCount = NULL;
     SSL *ssl_ = J2P(ssl, SSL *);
     if (ssl_ == NULL) {
         tcn_ThrowException(e, "ssl is null");
@@ -1759,11 +1772,11 @@ TCN_IMPLEMENT_CALL(jint, SSL, getHandshakeCount)(TCN_STDARGS, jlong ssl)
     }
     UNREFERENCED(o);
 
-    c = SSL_get_app_data2(ssl_);
-    if (c == NULL) {
-        return 0;
+    handshakeCount = SSL_get_app_data3(ssl_);
+    if (handshakeCount != NULL) {
+        return *handshakeCount;
     }
-    return c->handshakeCount;
+    return 0;
 }
 
 /*** End Apple API Additions ***/

src/main/c/ssl_private.h
@@ -283,9 +283,6 @@ struct tcn_ssl_ctxt_t {
     unsigned char   *alpn_proto_data;
     unsigned int    alpn_proto_len;
     int             alpn_selector_failure_behavior;
-
-    /* Number of handshakes */
-    int             handshakeCount;
 };
 
   
@@ -319,9 +316,13 @@ typedef struct {
 /*
  *  Additional Functions
  */
-void        SSL_init_app_data2_idx(void);
+void        SSL_init_app_data2_3_idx(void);
+// The app_data2 is used to store the tcn_ssl_ctxt_t pointer for the SSL instance.
 void       *SSL_get_app_data2(SSL *);
 void        SSL_set_app_data2(SSL *, void *);
+// The app_data3 is used to store the handshakeCount pointer for the SSL instance.
+void       *SSL_get_app_data3(SSL *);
+void        SSL_set_app_data3(SSL *, void *);
 int         SSL_password_prompt(tcn_pass_cb_t *);
 int         SSL_password_callback(char *, int, int, void *);
 void        SSL_BIO_close(BIO *);

src/main/c/sslutils.c
@@ -55,8 +55,8 @@ static int ssl_ocsp_request(X509 *cert, X509 *issuer);
  * SSL_get_ex_new_index() is called, so we _must_ do this at startup.
  */
 static int SSL_app_data2_idx = -1;
-
-void SSL_init_app_data2_idx(void)
+static int SSL_app_data3_idx = -1;
+void SSL_init_app_data2_3_idx(void)
 {
     int i;
 
@@ -64,13 +64,22 @@ void SSL_init_app_data2_idx(void)
         return;
     }
 
-    /* we _do_ need to call this twice */
+    /* we _do_ need to call this two times */
     for (i = 0; i <= 1; i++) {
         SSL_app_data2_idx =
             SSL_get_ex_new_index(0,
                                  "Second Application Data for SSL",
                                  NULL, NULL, NULL);
     }
+
+    if (SSL_app_data3_idx > -1) {
+        return;
+    }
+
+    SSL_app_data3_idx =
+            SSL_get_ex_new_index(0,
+                                 "Third Application Data for SSL",
+                                  NULL, NULL, NULL);
 }
 
 void *SSL_get_app_data2(SSL *ssl)
@@ -82,6 +91,16 @@ void SSL_set_app_data2(SSL *ssl, void *arg)
 {
     SSL_set_ex_data(ssl, SSL_app_data2_idx, (char *)arg);
     return;
+}
+
+void *SSL_get_app_data3(SSL *ssl)
+{
+    return SSL_get_ex_data(ssl, SSL_app_data3_idx);
+}
+
+void SSL_set_app_data3(SSL *ssl, void *arg)
+{
+    SSL_set_ex_data(ssl, SSL_app_data3_idx, arg);
 }
 
 /* Simple echo password prompting */

commit b7699c0a88d025529ec4000827c4f73e5ee6431e

Fix build problems on windows introduced by 2c17755b6330a6b6f2aa0dcb0ced5bca0ab476b3.

src/main/c/ssl.c
@@ -25,7 +25,6 @@
 #include "apr_thread_mutex.h"
 #include "apr_atomic.h"
 #include "apr_poll.h"
-
 #ifdef HAVE_OPENSSL
 #include "ssl_private.h"
 
@@ -1178,9 +1177,9 @@ TCN_IMPLEMENT_CALL(jint, SSL, getLastErrorNumber)(TCN_STDARGS) {
 }
 
 static void ssl_info_callback(const SSL *ssl, int where, int ret) {
-    uint16_t *handshakeCount = NULL;
+    int *handshakeCount = NULL;
     if (0 != (where & SSL_CB_HANDSHAKE_START)) {
-        handshakeCount = (uint16_t*) SSL_get_app_data3(ssl);
+        handshakeCount = (int*) SSL_get_app_data3(ssl);
         if (handshakeCount != NULL) {
             ++(*handshakeCount);
         }
@@ -1191,7 +1190,7 @@ TCN_IMPLEMENT_CALL(jlong /* SSL * */, SSL, newSSL)(TCN_STDARGS,
                                                    jlong ctx /* tcn_ssl_ctxt_t * */,
                                                    jboolean server) {
     tcn_ssl_ctxt_t *c = J2P(ctx, tcn_ssl_ctxt_t *);
-    uint16_t *handshakeCount = malloc(sizeof(uint16_t));
+    int *handshakeCount = malloc(sizeof(int));
     SSL *ssl;
 
     UNREFERENCED_STDARGS;
@@ -1317,16 +1316,15 @@ TCN_IMPLEMENT_CALL(void, SSL, setShutdown)(TCN_STDARGS,
 // Free the SSL * and its associated internal BIO
 TCN_IMPLEMENT_CALL(void, SSL, freeSSL)(TCN_STDARGS,
                                        jlong ssl /* SSL * */) {
+    SSL *ssl_ = J2P(ssl, SSL *);
+    int *handshakeCount = SSL_get_app_data3(ssl_);
+
     UNREFERENCED_STDARGS;
 
-    uint16_t *handshakeCount = NULL;
-    SSL *ssl_ = J2P(ssl, SSL *);
-
-    handshakeCount = SSL_get_app_data3(ssl_);
     if (handshakeCount != NULL) {
         free(handshakeCount);
     }
-    SSL_free(ssl);
+    SSL_free(ssl_);
 }
 
 // Make a BIO pair (network and internal) for the provided SSL * and return the network BIO
@@ -1764,7 +1762,7 @@ TCN_IMPLEMENT_CALL(jbyteArray, SSL, getSessionId)(TCN_STDARGS, jlong ssl)
 
 TCN_IMPLEMENT_CALL(jint, SSL, getHandshakeCount)(TCN_STDARGS, jlong ssl)
 {
-    uint16_t *handshakeCount = NULL;
+    int *handshakeCount = NULL;
     SSL *ssl_ = J2P(ssl, SSL *);
     if (ssl_ == NULL) {
         tcn_ThrowException(e, "ssl is null");