dbeea6d80a3cb683fa515663b9a81d1baf1c7f52
David Benjamin <davidben@google.com>
2016-10-19 11:25:42 -0400

    Remove unused constants.
    Motivation:
    
    There are a number of constants, probably some dating to Tomcat Native
    and some dating to before the code cleanup in
    7303f307239edc73cc4e8b99730c38e8e4f50ed6, that are unused.
    
    Modifications:
    
    Various unused constants removed from C and Java. Each of these
    constants are netty-tcnative constants, not transcribed OpenSSL ones, so
    if they are not processed within netty-tcnative, they can't be processed
    at all.
    
    Result:
    
    No behavior change. Less code.

--- openssl-dynamic/src/main/c/ssl_private.h
+++ openssl-dynamic/src/main/c/ssl_private.h
@@ -76,11 +76,6 @@
 #define RAND_MAX INT_MAX
 #endif
 
-#define SSL_ALGO_UNKNOWN (0)
-#define SSL_ALGO_RSA     (1<<0)
-#define SSL_ALGO_DSA     (1<<1)
-#define SSL_ALGO_ALL     (SSL_ALGO_RSA|SSL_ALGO_DSA)
-
 /*
  * Define IDs for the temporary RSA keys and DH params
  */
@@ -90,31 +85,6 @@
 #define SSL_TMP_KEY_DH_2048     (3)
 #define SSL_TMP_KEY_DH_4096     (4)
 #define SSL_TMP_KEY_MAX         (5)
-
-#define SSL_CRT_FORMAT_UNDEF    (0)
-#define SSL_CRT_FORMAT_ASN1     (1)
-#define SSL_CRT_FORMAT_TEXT     (2)
-#define SSL_CRT_FORMAT_PEM      (3)
-#define SSL_CRT_FORMAT_NETSCAPE (4)
-#define SSL_CRT_FORMAT_PKCS12   (5)
-#define SSL_CRT_FORMAT_SMIME    (6)
-#define SSL_CRT_FORMAT_ENGINE   (7)
-/* XXX this stupid macro helps us to avoid
- * adding yet another param to load_*key()
- */
-#define SSL_KEY_FORMAT_IISSGC   (8)
-
-/*
- * Define the SSL options
- */
-#define SSL_OPT_NONE            (0)
-#define SSL_OPT_RELSET          (1<<0)
-#define SSL_OPT_STDENVVARS      (1<<1)
-#define SSL_OPT_EXPORTCERTDATA  (1<<3)
-#define SSL_OPT_FAKEBASICAUTH   (1<<4)
-#define SSL_OPT_STRICTREQUIRE   (1<<5)
-#define SSL_OPT_OPTRENEGOTIATE  (1<<6)
-#define SSL_OPT_ALL             (SSL_OPT_STDENVVARS|SSL_OPT_EXPORTCERTDATA|SSL_OPT_FAKEBASICAUTH|SSL_OPT_STRICTREQUIRE|SSL_OPT_OPTRENEGOTIATE)
 
 /*
  * Define the SSL Protocol options
@@ -133,11 +103,8 @@
 #define SSL_MODE_SERVER         (1)
 #define SSL_MODE_COMBINED       (2)
 
-#define SSL_BIO_FLAG_RDONLY     (1<<0)
-#define SSL_BIO_FLAG_CALLBACK   (1<<1)
 #define SSL_DEFAULT_CACHE_SIZE  (256)
 #define SSL_DEFAULT_VHOST_NAME  ("_default_:443")
-#define SSL_MAX_STR_LEN         (2048)
 
 #define SSL_CVERIFY_UNSET           (-1)
 #define SSL_CVERIFY_NONE            (0)
@@ -148,55 +115,6 @@
 
 #define SSL_TO_APR_ERROR(X)         (APR_OS_START_USERERR + 1000 + X)
 
-#define SSL_INFO_SESSION_ID                 (0x0001)
-#define SSL_INFO_CIPHER                     (0x0002)
-#define SSL_INFO_CIPHER_USEKEYSIZE          (0x0003)
-#define SSL_INFO_CIPHER_ALGKEYSIZE          (0x0004)
-#define SSL_INFO_CIPHER_VERSION             (0x0005)
-#define SSL_INFO_CIPHER_DESCRIPTION         (0x0006)
-#define SSL_INFO_PROTOCOL                   (0x0007)
-
-#define SSL_INFO_CLIENT_S_DN                (0x0010)
-#define SSL_INFO_CLIENT_I_DN                (0x0020)
-#define SSL_INFO_SERVER_S_DN                (0x0040)
-#define SSL_INFO_SERVER_I_DN                (0x0080)
-
-#define SSL_INFO_DN_COUNTRYNAME             (0x0001)
-#define SSL_INFO_DN_STATEORPROVINCENAME     (0x0002)
-#define SSL_INFO_DN_LOCALITYNAME            (0x0003)
-#define SSL_INFO_DN_ORGANIZATIONNAME        (0x0004)
-#define SSL_INFO_DN_ORGANIZATIONALUNITNAME  (0x0005)
-#define SSL_INFO_DN_COMMONNAME              (0x0006)
-#define SSL_INFO_DN_TITLE                   (0x0007)
-#define SSL_INFO_DN_INITIALS                (0x0008)
-#define SSL_INFO_DN_GIVENNAME               (0x0009)
-#define SSL_INFO_DN_SURNAME                 (0x000A)
-#define SSL_INFO_DN_DESCRIPTION             (0x000B)
-#define SSL_INFO_DN_UNIQUEIDENTIFIER        (0x000C)
-#define SSL_INFO_DN_EMAILADDRESS            (0x000D)
-
-#define SSL_INFO_CLIENT_MASK                (0x0100)
-
-#define SSL_INFO_CLIENT_M_VERSION           (0x0101)
-#define SSL_INFO_CLIENT_M_SERIAL            (0x0102)
-#define SSL_INFO_CLIENT_V_START             (0x0103)
-#define SSL_INFO_CLIENT_V_END               (0x0104)
-#define SSL_INFO_CLIENT_A_SIG               (0x0105)
-#define SSL_INFO_CLIENT_A_KEY               (0x0106)
-#define SSL_INFO_CLIENT_CERT                (0x0107)
-#define SSL_INFO_CLIENT_V_REMAIN            (0x0108)
-
-#define SSL_INFO_SERVER_MASK                (0x0200)
-
-#define SSL_INFO_SERVER_M_VERSION           (0x0201)
-#define SSL_INFO_SERVER_M_SERIAL            (0x0202)
-#define SSL_INFO_SERVER_V_START             (0x0203)
-#define SSL_INFO_SERVER_V_END               (0x0204)
-#define SSL_INFO_SERVER_A_SIG               (0x0205)
-#define SSL_INFO_SERVER_A_KEY               (0x0206)
-#define SSL_INFO_SERVER_CERT                (0x0207)
-#define SSL_INFO_CLIENT_CERT_CHAIN          (0x0400)
-
 #define SSL_VERIFY_ERROR_IS_OPTIONAL(errnum) \
    ((errnum == X509_V_ERR_DEPTH_ZERO_SELF_SIGNED_CERT) \
     || (errnum == X509_V_ERR_SELF_SIGNED_CERT_IN_CHAIN) \
@@ -204,10 +122,6 @@
     || (errnum == X509_V_ERR_CERT_UNTRUSTED) \
     || (errnum == X509_V_ERR_UNABLE_TO_VERIFY_LEAF_SIGNATURE))
 
-
-#define OCSP_STATUS_OK        0
-#define OCSP_STATUS_REVOKED   1
-#define OCSP_STATUS_UNKNOWN   2
 
 #define MAX_ALPN_NPN_PROTO_SIZE 65535
 
