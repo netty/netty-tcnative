8cd4ed2bcc0213158314cae29bdbae7e335ad315
David Benjamin <davidben@google.com>
2016-12-10 12:06:38 -0500

    Remove unused functions and macros from tcn.h.
    Motivation:
    
    Less code. None of these appear to be used.
    
    Modifications:
    
    Remove unused functions and macros from tcn.h.
    
    Result:
    
    Less code.

--- openssl-dynamic/src/main/c/tcn.h
+++ openssl-dynamic/src/main/c/tcn.h
@@ -58,7 +58,6 @@
 #endif
 // End includes
 
-#define TCN_CLASS_PATH  "io/netty/tcnative/jni/"
 #define TCN_PARENT_IDE  "TCN_PARENT_ID"
 
 #define UNREFERENCED(P)      (P) = (P)
@@ -77,9 +76,6 @@
 #define TCN_IMPLEMENT_CALL(RT, CL, FN)  \
     JNIEXPORT RT JNICALL Java_io_netty_tcnative_jni_##CL##_##FN
 
-#define TCN_IMPLEMENT_METHOD(RT, FN)    \
-    static RT method_##FN
-
 
 /* Private helper functions */
 void            tcn_Throw(JNIEnv *, const char *, ...);
@@ -87,11 +83,6 @@ void            tcn_ThrowException(JNIEnv *, const char *);
 void            tcn_ThrowAPRException(JNIEnv *, apr_status_t);
 jstring         tcn_new_string(JNIEnv *, const char *);
 jstring         tcn_new_stringn(JNIEnv *, const char *, size_t);
-jbyteArray      tcn_new_arrayb(JNIEnv *, const unsigned char *, size_t);
-jobjectArray    tcn_new_arrays(JNIEnv *env, size_t len);
-char           *tcn_get_string(JNIEnv *, jstring);
-char           *tcn_strdup(JNIEnv *, jstring);
-char           *tcn_pstrdup(JNIEnv *, jstring, apr_pool_t *);
 
 #define J2S(V)  c##V
 #define J2L(V)  p##V
@@ -104,9 +95,6 @@ char           *tcn_pstrdup(JNIEnv *, jstring, apr_pool_t *);
 
 #define TCN_FREE_CSTRING(V)      \
     if (c##V) (*e)->ReleaseStringUTFChars(e, V, c##V)
-
-#define TCN_ALLOC_JSTRING(V)     \
-    char *c##V = tcn_get_string(e, (V))
 
 #define AJP_TO_JSTRING(V)   (*e)->NewStringUTF((e), (V))
 
@@ -126,9 +114,6 @@ char           *tcn_pstrdup(JNIEnv *, jstring, apr_pool_t *);
         }                                       \
     TCN_END_MACRO
 
-#define TCN_THROW_OS_ERROR(E)   \
-    tcn_ThrowAPRException((E), apr_get_os_error())
-
 #define TCN_LOAD_CLASS(E, C, N, R)                  \
     TCN_BEGIN_MACRO                                 \
         jclass _##C = (*(E))->FindClass((E), N);    \
@@ -142,9 +127,6 @@ char           *tcn_pstrdup(JNIEnv *, jstring, apr_pool_t *);
 
 #define TCN_UNLOAD_CLASS(E, C)                      \
         (*(E))->DeleteGlobalRef((E), (C))
-
-#define TCN_IS_NULL(E, O)                           \
-        ((*(E))->IsSameObject((E), (O), NULL) == JNI_TRUE)
 
 #define TCN_GET_METHOD(E, C, M, N, S, R)            \
     TCN_BEGIN_MACRO                                 \
@@ -162,34 +144,6 @@ char           *tcn_pstrdup(JNIEnv *, jstring, apr_pool_t *);
         }                                           \
     TCN_END_MACRO
 
-#define TCN_MAX_METHODS 8
-
-#define TCN_MIN(a, b) ((a) < (b) ? (a) : (b))
-#define TCN_MAX(a, b) ((a) > (b) ? (a) : (b))
-
-#ifdef WIN32
-#define TCN_ALLOC_WSTRING(V)     \
-    jsize wl##V = (*e)->GetStringLength(e, V);   \
-    const jchar *ws##V = V ? (const jchar *)((*e)->GetStringChars(e, V, 0)) : NULL; \
-    jchar *w##V = NULL
-
-#define TCN_INIT_WSTRING(V)                                     \
-        w##V = (jchar *)malloc((wl##V + 1) * sizeof(jchar));    \
-        wcsncpy(w##V, ws##V, wl##V);                        \
-        w##V[wl##V] = 0
-
-#define TCN_FREE_WSTRING(V)      \
-    if (ws##V) (*e)->ReleaseStringChars(e, V, ws##V); \
-    if (ws##V) free (w##V)
-
-#define J2W(V)  w##V
-
-#endif
-
-
-/* Return global apr pool
- */
-apr_pool_t *tcn_get_global_pool(void);
 
 /* Return global String class
  */
@@ -198,10 +152,6 @@ jclass tcn_get_string_class(void);
 jclass tcn_get_byte_array_class();
 jfieldID tcn_get_key_material_certificate_chain_field();
 jfieldID tcn_get_key_material_private_key_field();
-
-/* Return global JVM initalized on JNI_OnLoad
- */
-JavaVM *tcn_get_java_vm(void);
 
 /* Get current thread JNIEnv
  */
