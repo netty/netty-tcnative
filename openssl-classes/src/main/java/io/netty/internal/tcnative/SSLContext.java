b2377e401c87374860aac137cc28c1dbc08fc90b
Norman Maurer <norman_maurer@apple.com>
2023-04-21 11:48:28 +0200

    Expose SSL_CTX_set_max_cert_list
    Motivation:
    
    Sometimes its necessary to increase the maximum number of bytes a certificate chain can be
    
    Modifications:
    
    Expose SSL_CTX_set_max_cert_list
    
    Result:
    
    Be able to increase the limit

--- openssl-classes/src/main/java/io/netty/internal/tcnative/SSLContext.java
+++ openssl-classes/src/main/java/io/netty/internal/tcnative/SSLContext.java
@@ -750,4 +750,14 @@ public final class SSLContext {
     }
 
     private static native boolean setCurvesList0(long ctx, String curves);
+
+    /**
+     * Set the maximum number of bytes for the certificate chain during handshake.
+     * See
+     * <a href="https://www.openssl.org/docs/man1.1.1/man3/SSL_CTX_set_max_cert_list.html">SSL_CTX_set_max_cert_list</a>
+     * for more details.
+     * @param ctx context to use
+     * @param size the maximum number of bbytes
+     */
+    public static native void setMaxCertList(long ctx, long size);
 }
