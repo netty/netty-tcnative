netty/netty-tcnative

commit 33b2338ce415be8d3d7cb0a9aab962d2456af3ee

Revert static linking with stdc++ (#824)
Motiviation:

In the past we removed the usage of apr by static linking against
stdc++. While at first this seemed like a good idea it has proven to
introduce problems on various platforms. Because of these problems we
should better revert the changes and just link against apr again.

Modifications:

- Revert "Fix memory leak when creating and destroying a lot of context
(#790)" fd8fb2a4db697734edf9834559de5b9fcc4f0574.
- Revert "Fix possible corruption / segfault when using atomics (#774)"
11ff708a5b5334e2d8b5acd26bc2ecd135c2ba24.
- Revert "Fix segfault which was caused by not creating the
ticket_keys_new atomic (#772)" 196f935a38489cfb8885fb3758be8f2fc423c0c9.
- Revert "Fix format usage" eecaaa8e4222de1af05f9ccda0324b7c50955c97.
- Revert "Link libstdc++ statically on linux"
a288f03722e33efedad1cc16bd27b6f71ba74787.
- Revert "rename functions for consistency"
7ee7804ac5b28fca020fba2724456e02852e8200.
- Revert "Remove dependency on apr by using c++11 / c++14 features"
8740651154d184c1dc90b9345e765edb42121119.
- Update APR version

Result:

Fixes https://github.com/netty/netty-tcnative/issues/798 ,
https://github.com/netty/netty-tcnative/issues/789

openssl-dynamic/src/main/c/sslcontext.c
@@ -31,11 +31,11 @@
 
 #include "tcn.h"
 
-#include "tcn_atomic.h"
-#include "tcn_lock_rw.h"
+#include "apr_thread_rwlock.h"
+#include "apr_atomic.h"
+
 #include "ssl_private.h"
 #include <stdint.h>
-#include <string.h>
 #include "sslcontext.h"
 #include "cert_compress.h"
 
@@ -62,9 +62,120 @@ static jmethodID sslPrivateKeyMethodDecryptTask_init;
 
 static const char* staticPackagePrefix = NULL;
 
+extern apr_pool_t *tcn_global_pool;
+
+static apr_status_t ssl_context_cleanup(void *data)
+{
+    tcn_ssl_ctxt_t *c = (tcn_ssl_ctxt_t *)data;
+    JNIEnv *e = NULL;
+
+    if (c != NULL) {
+        SSL_CTX_free(c->ctx); // this function is safe to call with NULL
+        c->ctx = NULL;
+
+        tcn_get_java_env(&e);
+
+#ifdef OPENSSL_IS_BORINGSSL
+        if (c->ssl_private_key_method != NULL) {
+            if (e != NULL) {
+                (*e)->DeleteGlobalRef(e, c->ssl_private_key_method);
+            }
+            c->ssl_private_key_method = NULL;
+        }
+        if (c->ssl_cert_compression_zlib_algorithm != NULL) {
+            if (e != NULL) {
+                (*e)->DeleteGlobalRef(e, c->ssl_cert_compression_zlib_algorithm);
+            }
+            c->ssl_cert_compression_zlib_algorithm = NULL;
+        }
+        if (c->ssl_cert_compression_brotli_algorithm != NULL) {
+            if (e != NULL) {
+                (*e)->DeleteGlobalRef(e, c->ssl_cert_compression_brotli_algorithm);
+            }
+            c->ssl_cert_compression_brotli_algorithm = NULL;
+        }
+        if (c->ssl_cert_compression_zstd_algorithm != NULL) {
+            if (e != NULL) {
+                (*e)->DeleteGlobalRef(e, c->ssl_cert_compression_zstd_algorithm);
+            }
+            c->ssl_cert_compression_zstd_algorithm = NULL;
+        }
+#endif // OPENSSL_IS_BORINGSSL
+
+        if (c->ssl_session_cache != NULL) {
+            if (e != NULL) {
+                (*e)->DeleteGlobalRef(e, c->ssl_session_cache);
+            }
+            c->ssl_session_cache = NULL;
+        }
+        c->ssl_session_cache_creation_method = NULL;
+        c->ssl_session_cache_get_method = NULL;
+
+        if (c->verifier != NULL) {
+            if (e != NULL) {
+                (*e)->DeleteGlobalRef(e, c->verifier);
+            }
+            c->verifier = NULL;
+        }
+        c->verifier_method = NULL;
+
+        if (c->cert_requested_callback != NULL) {
+            if (e != NULL) {
+                (*e)->DeleteGlobalRef(e, c->cert_requested_callback);
+            }
+            c->cert_requested_callback = NULL;
+        }
+        c->cert_requested_callback_method = NULL;
+
+        if (c->certificate_callback != NULL) {
+            if (e != NULL) {
+                (*e)->DeleteGlobalRef(e, c->certificate_callback);
+            }
+            c->certificate_callback = NULL;
+        }
+        c->certificate_callback_method = NULL;
+
+        if (c->sni_hostname_matcher != NULL) {
+            if (e != NULL) {
+                (*e)->DeleteGlobalRef(e, c->sni_hostname_matcher);
+            }
+            c->sni_hostname_matcher = NULL;
+        }
+        c->sni_hostname_matcher_method = NULL;
+
+        if (c->next_proto_data != NULL) {
+            OPENSSL_free(c->next_proto_data);
+            c->next_proto_data = NULL;
+        }
+        c->next_proto_len = 0;
+
+        if (c->alpn_proto_data != NULL) {
+            OPENSSL_free(c->alpn_proto_data);
+            c->alpn_proto_data = NULL;
+        }
+        c->alpn_proto_len = 0;
+
+        apr_thread_rwlock_destroy(c->mutex);
+
+        if (c->ticket_keys != NULL) {
+            OPENSSL_free(c->ticket_keys);
+            c->ticket_keys = NULL;
+        }
+        c->ticket_keys_len = 0;
+
+        if (c->password != NULL) {
+            // Just use free(...) as we used strdup(...) to create the stored password.
+            free(c->password);
+            c->password = NULL;
+        }
+    }
+    return APR_SUCCESS;
+}
+
 /* Initialize server context */
 TCN_IMPLEMENT_CALL(jlong, SSLContext, make)(TCN_STDARGS, jint protocol, jint mode)
 {
+    apr_pool_t *p = NULL;
     tcn_ssl_ctxt_t *c = NULL;
     SSL_CTX *ctx = NULL;
 
@@ -226,18 +337,17 @@ TCN_IMPLEMENT_CALL(jlong, SSLContext, make)(TCN_STDARGS, jint protocol, jint mod
         goto cleanup;
     }
 
-    if ((c = calloc(1, sizeof(tcn_ssl_ctxt_t))) == NULL) {
-        tcn_Throw(e, "Failed to calloc tcn_ssl_ctxt_t");
+    TCN_THROW_IF_ERR(apr_pool_create(&p, tcn_global_pool), p);
+
+    if ((c = apr_pcalloc(p, sizeof(tcn_ssl_ctxt_t))) == NULL) {
+        tcn_ThrowAPRException(e, apr_get_os_error());
         goto cleanup;
     }
 
     c->protocol = protocol;
     c->mode     = mode;
     c->ctx      = ctx;
-    c->ticket_keys_new = tcn_atomic_uint32_create();
-    c->ticket_keys_resume = tcn_atomic_uint32_create();
-    c->ticket_keys_renew = tcn_atomic_uint32_create();
-    c->ticket_keys_fail = tcn_atomic_uint32_create();
+    c->pool     = p;
 
     if (!(protocol & SSL_PROTOCOL_SSLV2)) {
         SSL_CTX_set_options(c->ctx, SSL_OP_NO_SSLv2);
@@ -323,11 +433,20 @@ TCN_IMPLEMENT_CALL(jlong, SSLContext, make)(TCN_STDARGS, jint protocol, jint mod
         SSL_CTX_set_allow_unknown_alpn_protos(ctx, 1);
     }
 #endif
-    c->ticket_keys_lock = tcn_lock_rw_create();
+    apr_thread_rwlock_create(&c->mutex, p);
+    /*
+     * Let us cleanup the ssl context when the pool is destroyed
+     */
+    apr_pool_cleanup_register(p, (const void *)c,
+                              ssl_context_cleanup,
+                              apr_pool_cleanup_null);
 
     tcn_SSL_CTX_set_app_state(c->ctx, c);
     return P2J(c);
 cleanup:
+    if (p != NULL) {
+        apr_pool_destroy(p);
+    }
     SSL_CTX_free(ctx); // this function is safe to call with NULL.
     return 0;
 }
@@ -338,108 +457,10 @@ TCN_IMPLEMENT_CALL(jint, SSLContext, free)(TCN_STDARGS, jlong ctx)
 
     TCN_CHECK_NULL(c, ctx, 0);
 
-    SSL_CTX_free(c->ctx); // this function is safe to call with NULL
-    c->ctx = NULL;
-
-#ifdef OPENSSL_IS_BORINGSSL
-    if (c->ssl_private_key_method != NULL) {
-        (*e)->DeleteGlobalRef(e, c->ssl_private_key_method);
-        c->ssl_private_key_method = NULL;
-    }
-    if (c->ssl_cert_compression_zlib_algorithm != NULL) {
-        (*e)->DeleteGlobalRef(e, c->ssl_cert_compression_zlib_algorithm);
-        c->ssl_cert_compression_zlib_algorithm = NULL;
-    }
-    if (c->ssl_cert_compression_brotli_algorithm != NULL) {
-        (*e)->DeleteGlobalRef(e, c->ssl_cert_compression_brotli_algorithm);
-        c->ssl_cert_compression_brotli_algorithm = NULL;
-    }
-    if (c->ssl_cert_compression_zstd_algorithm != NULL) {
-        (*e)->DeleteGlobalRef(e, c->ssl_cert_compression_zstd_algorithm);
-        c->ssl_cert_compression_zstd_algorithm = NULL;
-    }
-#endif // OPENSSL_IS_BORINGSSL
-
-    if (c->ssl_session_cache != NULL) {
-        (*e)->DeleteGlobalRef(e, c->ssl_session_cache);
-        c->ssl_session_cache = NULL;
-    }
-    c->ssl_session_cache_creation_method = NULL;
-    c->ssl_session_cache_get_method = NULL;
-
-    if (c->verifier != NULL) {
-        (*e)->DeleteGlobalRef(e, c->verifier);
-        c->verifier = NULL;
-    }
-    c->verifier_method = NULL;
-
-    if (c->cert_requested_callback != NULL) {
-        (*e)->DeleteGlobalRef(e, c->cert_requested_callback);
-        c->cert_requested_callback = NULL;
-    }
-    c->cert_requested_callback_method = NULL;
-
-    if (c->certificate_callback != NULL) {
-        (*e)->DeleteGlobalRef(e, c->certificate_callback);
-        c->certificate_callback = NULL;
-    }
-    c->certificate_callback_method = NULL;
-
-    if (c->sni_hostname_matcher != NULL) {
-        (*e)->DeleteGlobalRef(e, c->sni_hostname_matcher);
-        c->sni_hostname_matcher = NULL;
-    }
-    c->sni_hostname_matcher_method = NULL;
-
-    if (c->next_proto_data != NULL) {
-        OPENSSL_free(c->next_proto_data);
-        c->next_proto_data = NULL;
-    }
-    c->next_proto_len = 0;
-
-    if (c->alpn_proto_data != NULL) {
-        OPENSSL_free(c->alpn_proto_data);
-        c->alpn_proto_data = NULL;
-    }
-    c->alpn_proto_len = 0;
-
-    if (c->ticket_keys_lock != NULL) {
-        tcn_lock_rw_destroy(c->ticket_keys_lock);
-        c->ticket_keys_lock = NULL;
-    }
-
-    if (c->ticket_keys_new != NULL) {
-        tcn_atomic_uint32_destroy(c->ticket_keys_new);
-        c->ticket_keys_new = NULL;
-    }
-    if (c->ticket_keys_resume != NULL) {
-        tcn_atomic_uint32_destroy(c->ticket_keys_resume);
-        c->ticket_keys_resume = NULL;
-    }
-    if (c->ticket_keys_renew != NULL) {
-        tcn_atomic_uint32_destroy(c->ticket_keys_renew);
-        c->ticket_keys_renew = NULL;
-    }
-    if (c->ticket_keys_fail != NULL) {
-        tcn_atomic_uint32_destroy(c->ticket_keys_fail);
-        c->ticket_keys_fail = NULL;
-    }
-
-    if (c->ticket_keys != NULL) {
-        OPENSSL_free(c->ticket_keys);
-        c->ticket_keys = NULL;
-    }
-    c->ticket_keys_len = 0;
-
-    if (c->password != NULL) {
-        // Just use free(...) as we used strdup(...) to create the stored password.
-        free(c->password);
-        c->password = NULL;
-    }
-
-    // Use free as we used calloc(...) to allocate
-    free(c);
-    return 0;
+    /* Run and destroy the cleanup callback */
+    int result = apr_pool_cleanup_run(c->pool, c, ssl_context_cleanup);
+    apr_pool_destroy(c->pool);
+    return result;
 }
 
 TCN_IMPLEMENT_CALL(void, SSLContext, setContextId)(TCN_STDARGS, jlong ctx,
@@ -644,7 +665,7 @@ TCN_IMPLEMENT_CALL(void, SSLContext, setTmpDHLength)(TCN_STDARGS, jlong ctx, jin
             SSL_CTX_set_tmp_dh_callback(c->ctx, tcn_SSL_callback_tmp_DH_4096);
             return;
         default:
-            tcn_Throw(e, "Unsupported length %d", length);
+            tcn_Throw(e, "Unsupported length %s", length);
             return;
     }
 }
@@ -1187,7 +1208,8 @@ TCN_IMPLEMENT_CALL(jlong, SSLContext, sessionTicketKeyNew)(TCN_STDARGS, jlong ct
 
     TCN_CHECK_NULL(c, ctx, 0);
 
-    return (jlong) tcn_atomic_uint32_get(c->ticket_keys_new);
+    jlong rv = apr_atomic_read32(&c->ticket_keys_new);
+    return rv;
 }
 
 TCN_IMPLEMENT_CALL(jlong, SSLContext, sessionTicketKeyResume)(TCN_STDARGS, jlong ctx)
@@ -1196,7 +1218,8 @@ TCN_IMPLEMENT_CALL(jlong, SSLContext, sessionTicketKeyResume)(TCN_STDARGS, jlong
 
     TCN_CHECK_NULL(c, ctx, 0);
 
-    return (jlong) tcn_atomic_uint32_get(c->ticket_keys_resume);
+    jlong rv = apr_atomic_read32(&c->ticket_keys_resume);
+    return rv;
 }
 
 TCN_IMPLEMENT_CALL(jlong, SSLContext, sessionTicketKeyRenew)(TCN_STDARGS, jlong ctx)
@@ -1205,7 +1228,8 @@ TCN_IMPLEMENT_CALL(jlong, SSLContext, sessionTicketKeyRenew)(TCN_STDARGS, jlong 
 
     TCN_CHECK_NULL(c, ctx, 0);
 
-    return (jlong) tcn_atomic_uint32_get(c->ticket_keys_renew);
+    jlong rv = apr_atomic_read32(&c->ticket_keys_renew);
+    return rv;
 }
 
 TCN_IMPLEMENT_CALL(jlong, SSLContext, sessionTicketKeyFail)(TCN_STDARGS, jlong ctx)
@@ -1214,17 +1238,18 @@ TCN_IMPLEMENT_CALL(jlong, SSLContext, sessionTicketKeyFail)(TCN_STDARGS, jlong c
 
     TCN_CHECK_NULL(c, ctx, 0);
 
-    return (jlong) tcn_atomic_uint32_get(c->ticket_keys_fail);
+    jlong rv = apr_atomic_read32(&c->ticket_keys_fail);
+    return rv;
 }
 
 static int current_session_key(tcn_ssl_ctxt_t *c, tcn_ssl_ticket_key_t *key) {
     int result = JNI_FALSE;
-    tcn_lock_r_t reader_lock = tcn_lock_r_acquire(c->ticket_keys_lock);
+    apr_thread_rwlock_rdlock(c->mutex);
     if (c->ticket_keys_len > 0) {
         *key = c->ticket_keys[0];
         result = JNI_TRUE;
     }
-    tcn_lock_r_release(reader_lock);
+    apr_thread_rwlock_unlock(c->mutex);
     return result;
 }
 
@@ -1232,7 +1257,7 @@ static int find_session_key(tcn_ssl_ctxt_t *c, unsigned char key_name[16], tcn_s
     int result = JNI_FALSE;
     int i;
 
-    tcn_lock_r_t reader_lock = tcn_lock_r_acquire(c->ticket_keys_lock);
+    apr_thread_rwlock_rdlock(c->mutex);
     for (i = 0; i < c->ticket_keys_len; ++i) {
         // Check if we have a match for tickets.
         if (memcmp(c->ticket_keys[i].key_name, key_name, 16) == 0) {
@@ -1242,7 +1267,7 @@ static int find_session_key(tcn_ssl_ctxt_t *c, unsigned char key_name[16], tcn_s
             break;
         }
     }
-    tcn_lock_r_release(reader_lock);
+    apr_thread_rwlock_unlock(c->mutex);
     return result;
 }
 
@@ -1265,9 +1290,7 @@ static int ssl_tlsext_ticket_key_cb(SSL *s, unsigned char key_name[16], unsigned
 
              EVP_EncryptInit_ex(ctx, EVP_aes_128_cbc(), NULL, key.aes_key, iv);
              HMAC_Init_ex(hctx, key.hmac_key, 16, EVP_sha256(), NULL);
-             if (c->ticket_keys_new != NULL) {
-                 tcn_atomic_uint32_increment(c->ticket_keys_new);
-             }
+             apr_atomic_inc32(&c->ticket_keys_new);
              return 1;
          }
          // No ticket configured
@@ -1279,21 +1302,15 @@ static int ssl_tlsext_ticket_key_cb(SSL *s, unsigned char key_name[16], unsigned
              if (!is_current_key) {
                  // The ticket matched a key in the list, and we want to upgrade it to the current
                  // key.
-                 if (c->ticket_keys_renew != NULL) {
-                     tcn_atomic_uint32_increment(c->ticket_keys_renew);
-                 }
+                 apr_atomic_inc32(&c->ticket_keys_renew);
                  return 2;
              }
              // The ticket matched the current key.
-             if (c->ticket_keys_resume != NULL) {
-                tcn_atomic_uint32_increment(c->ticket_keys_resume);
-             }
+             apr_atomic_inc32(&c->ticket_keys_resume);
              return 1;
          }
          // No matching ticket.
-         if (c->ticket_keys_fail != NULL) {
-             tcn_atomic_uint32_increment(c->ticket_keys_fail);
-         }
+         apr_atomic_inc32(&c->ticket_keys_fail);
          return 0;
      }
 }
@@ -1325,13 +1342,13 @@ TCN_IMPLEMENT_CALL(void, SSLContext, setSessionTicketKeys0)(TCN_STDARGS, jlong c
     }
     (*e)->ReleaseByteArrayElements(e, keys, b, 0);
 
-    tcn_lock_w_t writer_lock = tcn_lock_w_acquire(c->ticket_keys_lock);
+    apr_thread_rwlock_wrlock(c->mutex);
     if (c->ticket_keys) {
         OPENSSL_free(c->ticket_keys);
     }
     c->ticket_keys_len = cnt;
     c->ticket_keys = ticket_keys;
-    tcn_lock_w_release(writer_lock);
+    apr_thread_rwlock_unlock(c->mutex);
 
     SSL_CTX_set_tlsext_ticket_key_cb(c->ctx, ssl_tlsext_ticket_key_cb);
 }

.github/workflows/codeql-analysis.yml
@@ -71,7 +71,7 @@ jobs:
     #    uses a compiled language
 
     - name: Install tools / libraries 
-      run: sudo apt-get update && sudo apt-get -y install autoconf automake libtool libtool-bin make tar libssl-dev cmake perl ninja-build
+      run: sudo apt-get update && sudo apt-get -y install autoconf automake libtool libtool-bin make tar libapr1-dev libssl-dev cmake perl ninja-build
 
     - name: Build project
       run: ./mvnw clean package -pl openssl-dynamic -DskipTests=true

Brewfile
@@ -6,3 +6,4 @@ brew 'perl'
 brew 'ninja'
 brew 'golang'
 brew 'cmake'
+brew 'apr'

boringssl-static/pom.xml
@@ -27,7 +27,7 @@
   <name>Netty/TomcatNative [BoringSSL - Static]</name>
   <description>
     A Mavenized fork of Tomcat Native which incorporates various patches. This artifact is statically linked
-    to BoringSSL.
+    to BoringSSL and Apache APR.
   </description>
 
   <build>
@@ -130,6 +130,7 @@
             <artifactId>maven-bundle-plugin</artifactId>
             <configuration>
               <instructions>
+                <Apr-Version>${aprVersion}</Apr-Version>
                 <BoringSSL-Revision>${boringsslCommitSha}</BoringSSL-Revision>
                 <BoringSSL-Branch>${boringsslBranch}</BoringSSL-Branch>
               </instructions>
@@ -317,7 +318,7 @@
             </executions>
           </plugin>
 
-          <!-- Configure the distribution statically linked against OpenSSL -->
+          <!-- Configure the distribution statically linked against OpenSSL and APR -->
           <plugin>
             <groupId>org.fusesource.hawtjni</groupId>
             <artifactId>hawtjni-maven-plugin</artifactId>
@@ -340,6 +341,7 @@
                   <!-- <verbose>true</verbose> -->
                   <configureArgs>
                     <configureArg>--with-ssl=no</configureArg>
+                    <configureArg>--with-apr=${aprHome}</configureArg>
                     <configureArg>--with-static-libs</configureArg>
                     <configureArg>--libdir=${project.build.directory}/native-build/target/lib</configureArg>
                     <configureArg>${macOsxDeploymentTarget}</configureArg>
@@ -417,6 +419,10 @@
                       </files>
                       <content>release 7.6</content>
                     </requireFilesContent>
+                    <requireProperty>
+                      <property>aprArmHome</property>
+                      <message>The folder of APR for aarch64 must be specified by hand. Please try -DaprArmHome=</message>
+                    </requireProperty>
                   </rules>
                   <ignoreCache>true</ignoreCache>
                 </configuration>
@@ -469,6 +475,7 @@
             <artifactId>maven-bundle-plugin</artifactId>
             <configuration>
               <instructions>
+                <Apr-Version>${aprVersion}</Apr-Version>
                 <BoringSSL-Revision>${boringsslCommitSha}</BoringSSL-Revision>
                 <BoringSSL-Branch>${boringsslBranch}</BoringSSL-Branch>
               </instructions>
@@ -645,7 +652,7 @@
             </executions>
           </plugin>
 
-          <!-- Configure the distribution statically linked against OpenSSL -->
+          <!-- Configure the distribution statically linked against OpenSSL and APR -->
           <plugin>
             <groupId>org.fusesource.hawtjni</groupId>
             <artifactId>hawtjni-maven-plugin</artifactId>
@@ -668,6 +675,7 @@
                   <!-- <verbose>true</verbose> -->
                   <configureArgs>
                     <configureArg>--with-ssl=no</configureArg>
+                    <configureArg>--with-apr=${aprArmHome}</configureArg>
                     <configureArg>--with-static-libs</configureArg>
                     <configureArg>--libdir=${project.build.directory}/native-build/target/lib</configureArg>
                     <configureArg>CFLAGS=-O3 -Werror -fno-omit-frame-pointer -fvisibility=hidden -Wunused -Wno-unused-value</configureArg>
@@ -675,7 +683,6 @@
                     <configureArg>LDFLAGS=-L${boringsslHome}/ssl -L${boringsslHome}/crypto -lssl -lcrypto</configureArg>
                     <configureArg>--host=aarch64-linux-gnu</configureArg>
                     <configureArg>CC=aarch64-none-linux-gnu-gcc</configureArg>
-                    <configureArg>CXX=aarch64-none-linux-gnu-g++</configureArg>
                   </configureArgs>
                 </configuration>
               </execution>
@@ -928,6 +935,7 @@
             <artifactId>maven-bundle-plugin</artifactId>
             <configuration>
               <instructions>
+                <Apr-Version>${aprVersion}</Apr-Version>
                 <BoringSSL-Revision>${boringsslCommitSha}</BoringSSL-Revision>
                 <BoringSSL-Branch>${boringsslBranch}</BoringSSL-Branch>
               </instructions>
@@ -1063,7 +1071,7 @@
             </executions>
           </plugin>
 
-          <!-- Configure the distribution statically linked against OpenSSL -->
+          <!-- Configure the distribution statically linked against OpenSSL and APR -->
           <plugin>
             <groupId>org.fusesource.hawtjni</groupId>
             <artifactId>hawtjni-maven-plugin</artifactId>
@@ -1085,6 +1093,7 @@
                   <!-- <verbose>true</verbose> -->
                   <configureArgs>
                     <configureArg>--with-ssl=no</configureArg>
+                    <configureArg>--with-apr=${aprHome}</configureArg>
                     <configureArg>--with-static-libs</configureArg>
                     <configureArg>--libdir=${project.build.directory}/native-build/target/lib</configureArg>
                     <configureArg>--host=aarch64-apple-darwin</configureArg>
@@ -1173,6 +1182,7 @@
             <artifactId>maven-bundle-plugin</artifactId>
             <configuration>
               <instructions>
+                <Apr-Version>${aprVersion}</Apr-Version>
                 <BoringSSL-Revision>${boringsslCommitSha}</BoringSSL-Revision>
                 <BoringSSL-Branch>${boringsslBranch}</BoringSSL-Branch>
               </instructions>
@@ -1308,7 +1318,7 @@
             </executions>
           </plugin>
 
-          <!-- Configure the distribution statically linked against OpenSSL -->
+          <!-- Configure the distribution statically linked against OpenSSL and APR -->
           <plugin>
             <groupId>org.fusesource.hawtjni</groupId>
             <artifactId>hawtjni-maven-plugin</artifactId>
@@ -1330,6 +1340,7 @@
                   <!-- <verbose>true</verbose> -->
                   <configureArgs>
                     <configureArg>--with-ssl=no</configureArg>
+                    <configureArg>--with-apr=${aprHome}</configureArg>
                     <configureArg>--with-static-libs</configureArg>
                     <configureArg>--libdir=${project.build.directory}/native-build/target/lib</configureArg>
                     <configureArg>--host=x86_64-apple-darwin</configureArg>

docker/Dockerfile.arch
@@ -6,6 +6,7 @@ ENV JAVA_VERSION $java_version
 # install dependencies
 # use openSSL 1.0.x for now, for highest compatibility
 RUN pacman -Sy --noconfirm --needed \
+ apr \
  autoconf \
  automake \
  bzip2 \

docker/Dockerfile.centos6
@@ -12,12 +12,14 @@ RUN sed -i -e 's/^mirrorlist/#mirrorlist/g' -e 's/^#baseurl=http:\/\/mirror.cent
 
 # install dependencies
 RUN yum install -y \
+ apr-devel \
  autoconf \
  automake \
  bzip2 \
  git \
  glibc-devel \
  gnupg \
+ libapr1-dev \
  libtool \
  lsb-core \
  make \

docker/Dockerfile.cross_compile_aarch64
@@ -2,10 +2,12 @@ FROM centos:7.6.1810
 
 ARG gcc_version=10.2-2020.11
 ARG openssl_version=1_1_1d
+ARG apr_version=1.7.4
 ENV SOURCE_DIR /root/source
 ENV GCC_VERSION $gcc_version
 ENV OPENSSL_VERSION $openssl_version
 ENV MAVEN_VERSION 3.9.1
+ENV APR_VERSION $apr_version
 
 RUN mkdir $SOURCE_DIR
 WORKDIR $SOURCE_DIR
@@ -31,6 +33,32 @@ ENV PATH="/gcc-arm-$GCC_VERSION-x86_64-aarch64-none-linux-gnu/bin:${PATH}"
 
 # Install CMake
 RUN yum install -y cmake3 && ln -s /usr/bin/cmake3 /usr/bin/cmake
+
+# Cross compile Apache Apr for aarch64 - static
+RUN set -x && \
+  wget https://downloads.apache.org//apr/apr-$APR_VERSION.tar.gz && \
+  tar xvf apr-$APR_VERSION.tar.gz && \
+  pushd apr-$APR_VERSION && \
+  CC=aarch64-none-linux-gnu-gcc CFLAGS='-O3 -fno-omit-frame-pointer -fPIC' ./configure --disable-shared --prefix=/opt/apr-$APR_VERSION-static --host=aarch64-none-linux-gnu ac_cv_file__dev_zero=yes ac_cv_func_setpgrp_void=yes apr_cv_tcp_nodelay_with_cork=yes ac_cv_sizeof_struct_iovec=8 && \
+  make || true && \
+  pushd tools && \
+  gcc -Wall -O2 -DCROSS_COMPILE gen_test_char.c -s -o gen_test_char && \
+  popd && \
+  make && make install && \
+  popd
+
+# Cross compile Apache Apr for aarch64 - share
+RUN set -x && \
+  wget https://downloads.apache.org//apr/apr-$APR_VERSION.tar.gz && \
+  tar xvf apr-$APR_VERSION.tar.gz && \
+  pushd apr-$APR_VERSION && \
+  CC=aarch64-none-linux-gnu-gcc CFLAGS='-O3 -fno-omit-frame-pointer -fPIC' ./configure --prefix=/opt/apr-$APR_VERSION-share --host=aarch64-none-linux-gnu ac_cv_file__dev_zero=yes ac_cv_func_setpgrp_void=yes apr_cv_tcp_nodelay_with_cork=yes ac_cv_sizeof_struct_iovec=8 && \
+  make || true && \
+  pushd tools && \
+  gcc -Wall -O2 -DCROSS_COMPILE gen_test_char.c -s -o gen_test_char && \
+  popd && \
+  make && make install && \
+  popd
 
 # Cross compile OpenSSL for aarch64 - share
 RUN set -x && \

docker/Dockerfile.debian
@@ -24,6 +24,7 @@ RUN echo "deb http://archive.debian.org/debian/ wheezy contrib main non-free" > 
  git \
  gnupg \
  g++ \
+ libapr1-dev \
  libssl1.0.0=1.0.1e-2+deb7u20 \
  libssl-dev \
  libtool \

commit 10c925755d572c1905e16ef6d5ab903230bfa466

Return correct value from cert callback when no tasks are used
Motivation:

33b2338ce415be8d3d7cb0a9aab962d2456af3ee introduced a bug which lead to return the incorrect value when no tasks are used.

Modifications:

Correctly return 1 when succesful

Result:

Correctly signal back that the cert could be selected

openssl-dynamic/src/main/c/sslcontext.c
@@ -2016,7 +2016,7 @@ static int certificate_cb(SSL* ssl, void* arg) {
         // Check if java threw an exception and if so signal back that we should not continue with the handshake.
         if ((*e)->ExceptionCheck(e) != JNI_TRUE) {
             // Everything good...
-            ret = -1;
+            ret = 1;
         }
     }
 